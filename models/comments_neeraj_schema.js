93dbbbe29bdcc831a7470d528180b1918ab1d8bd
dauqu <sujeetsingh734321@gmail.com>
2023-01-21 16:40:50 +0530

    first commit

---
+++ models/comments_neeraj_schema.js
@@ -0,0 +1,42 @@
+//comment schemas
+
+const mongoose = require("mongoose")
+
+const commentsSchema= mongoose.Schema({
+    userid :{
+        type: String,
+    },
+    postid : {
+        type : Number,
+    },
+    comments : {
+        type : String, 
+    },
+    nestedcomments : [
+        {
+            userid:{
+                type:String,
+                required:true
+            },
+            realuser:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"User",
+                
+            },
+            comments:{
+                type:String,
+                required:true
+            },
+            date:{
+                type:Date,
+                required:true
+            },
+            comment_id:{
+                type:String,
+                required:true
+            },   
+        }
+    ]
+},{timestamps:true})
+
+module.exports= mongoose.model("comments", commentsSchema) 
